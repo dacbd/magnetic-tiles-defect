6dd1ae736595bb930f875185d0144d4922c38b60
Daniel Barnes <dabarnes2b@gmail.com>
2022-08-17 09:45:40 -0700

    add bb pipeline

---
+++ tpi-run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export TF_LOG_PROVIDER=INFO
+terraform init
+terraform apply -auto-approve
+
+until echo 'try(iterative_task.gpu-runner.status["succeeded"], 0) + try(iterative_task.gpu-runner.status["failed"], 0)' | terraform console | grep --quiet --invert-match 0; do
+    sleep 60
+    terraform refresh
+done
+
+terraform destroy -auto-approve
